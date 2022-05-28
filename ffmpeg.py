68e58dae9f36d091b49176c5f12ddf7a7e536c6f
ozabirnyk <106436338+ozabirnyk@users.noreply.github.com>
2022-05-28 16:26:15 +0300

    Update 0.1

--- ffmpeg.py
+++ ffmpeg.py
@@ -8,16 +8,17 @@ if not os.path.exists(output_dir):
 
 input_dir_list = os.listdir(work_dir)
 
-for sub_dir in work_dir_list:
+for sub_dir in input_dir_list:
     current_input_dir = input_dir + "\\" + sub_dir
-    if os.path.isdir(current_input_dir):
-        current_output_dir = output_dir + "\\" + sub_dir
-        if not os.path.exists(current_output_dir):
-            os.mkdir(current_output_dir)
-        current_input_dir_list = os.listdir(current_input_dir)
-        for input_video in current_input_dir_list:
-            input_video_full_path = current_input_dir + "\\" + input_video
-            if input_video_full_path[-4:].lower() == ".mp4":                # mp4 video
-                output_audio_full_path = current_output_dir + "\\" + input_video[:-4] + ".mp3"            # mp3 audio
-                ffmpeg_params = "-i \"" + input_video_full_path + "\" -y \"" + output_audio_full_path + "\""
-                !ffmpeg $ffmpeg_params
+    if not os.path.isdir(current_input_dir):
+        continue
+    current_output_dir = os.path.join(output_dir, sub_dir)
+    if not os.path.exists(current_output_dir):
+        os.mkdir(current_output_dir)
+    current_input_dir_list = os.listdir(current_input_dir)
+    for input_video in current_input_dir_list:
+        input_video_full_path = os.path.join(current_input_dir, input_video)
+        if input_video_full_path[-4:].lower() == ".mp4":                                                    # mp4 video
+            output_audio_full_path = os.path.join(current_output_dir, input_video[:-4] + ".mp3")            # mp3 audio
+            ffmpeg_params = f"-i \"{input_video_full_path}\" -y \"{output_audio_full_path}\""
+            !ffmpeg $ffmpeg_params
